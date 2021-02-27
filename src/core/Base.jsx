8d43c140583ef3882096018f35e5f35dcc94070b
Manikant Jha <mjontop0602@gmail.com>
2021-02-27 13:10:01 +0530

    initilized and navbar added

---
+++ src/core/Base.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Base = () => (
+  <>
+    <div className="row shadow p-3 mb-5 bg-white rounded">
+      <div className="col-md-2 col-12 py-4">spark icon img</div>
+      <div className="col-md-10 col-12 py-4">
+        <b>Laser Liposuction / Fat Transfer</b>
+      </div>
+    </div>
+    <div>
+      <div>asdf</div>
+    </div>
+  </>
+);
+
+export default Base;
