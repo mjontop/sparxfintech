2807e2943fb9449242fa3e9711f2685ff4a3822d
Manikant Jha <mjontop0602@gmail.com>
2021-03-07 06:48:46 +0530

    show hide button added

--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -125,10 +125,16 @@ export default function Home() {
     comp: "",
     type: "",
     keywords: "",
+    buttonStatus: false,
+    showStats: false,
   });
+
   const handleChange = (name) => (event) => {
     event.preventDefault();
     setDropDownDataOP({ ...dropDownDataOP, [name]: event.target.value });
+    if (dropDownDataOP.comp !== "" && dropDownDataOP.type !== "") {
+      setDropDownDataOP({ ...dropDownDataOP, buttonStatus: true });
+    }
   };
 
   const tableDatas = [
@@ -264,7 +270,10 @@ export default function Home() {
             </select>
           </div>
           <div className="col-md-3 col-12 my-1">
-            <select className="btn rounded10 border border-primary w-100 py-2">
+            <select
+              className="btn rounded10 border border-primary w-100 py-2"
+              onClick={handleChange("keywords")}
+            >
               <option value="">Keywords</option>
               {dropDownDataOP["type"] !== "" &&
                 dropdownData[dropDownDataOP["comp"]].map((data, index) => {
@@ -277,103 +286,116 @@ export default function Home() {
             </select>
           </div>
         </div>
-        <h3 className="mt-4">
-          <b>Laser Liposuction / Fat Transfer</b>
-        </h3>
-        <hr />
-        <span className="text-muted">Overview</span>
-        <div className="mt-4 border bg-white rounded10 p-4 p-md-5 shadow">
-          <table className="table">
-            <thead className="text-primary ">
-              <tr>
-                <th scope="col">Demographic</th>
-                <th scope="col">Zipcode</th>
-                <th scope="col">Market</th>
-                <th scope="col" className="text-dark">
-                  Market Variables
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableDatas.map((tableData, index) => (
-                <tr key={index}>
-                  <td scope="row">{tableData.demography}</td>
-                  <td>{tableData.zipcode}</td>
-                  <td>{tableData.market}</td>
-                  <td>
-                    <b className="font-raleway">
-                      {!!tableData.marketVariable &&
-                        tableData.marketVariable.desc}
-                    </b>
-                    <br />
-                    {!!tableData.marketVariable && (
-                      <Slider
-                        value={tableData.marketVariable.data}
-                        valueLabelDisplay="auto"
-                        aria-labelledby="range-slider"
-                      />
-                    )}
-                  </td>
-                </tr>
+        <button className="btn buttonBlue my-2 text-white" disabled={!dropDownDataOP.buttonStatus}
+        onClick={(e)=> {
+          e.preventDefault()
+          setDropDownDataOP({...dropDownDataOP, showStats: !dropDownDataOP.showStats})
+        }}
+        >{!dropDownDataOP.showStats? 'Show':'Hide'} Stats</button>
+        {dropDownDataOP.showStats && (
+          <>
+            <h3 className="mt-4">
+              <b>Laser Liposuction / Fat Transfer</b>
+            </h3>
+            <hr />
+            <span className="text-muted">Overview</span>
+            <div className="mt-4 border bg-white rounded10 p-4 p-md-5 shadow">
+              <table className="table">
+                <thead className="text-primary ">
+                  <tr>
+                    <th scope="col">Demographic</th>
+                    <th scope="col">Zipcode</th>
+                    <th scope="col">Market</th>
+                    <th scope="col" className="text-dark">
+                      Market Variables
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableDatas.map((tableData, index) => (
+                    <tr key={index}>
+                      <td scope="row">{tableData.demography}</td>
+                      <td>{tableData.zipcode}</td>
+                      <td>{tableData.market}</td>
+                      <td>
+                        <b className="font-raleway">
+                          {!!tableData.marketVariable &&
+                            tableData.marketVariable.desc}
+                        </b>
+                        <br />
+                        {!!tableData.marketVariable && (
+                          <Slider
+                            value={tableData.marketVariable.data}
+                            valueLabelDisplay="auto"
+                            aria-labelledby="range-slider"
+                          />
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="row mt-4">
+              {annualCarddata.map((data, index) => (
+                <div
+                  className="col-md-6 col-lg-4 my-1 col-12 shadow mx-auto rounded10 bg-white"
+                  key={index}
+                  style={{ maxWidth: "400px" }}
+                >
+                  <div className="p-2 row  m-1">
+                    <div className="w-100"></div>
+                    <div className="col-6">
+                      {circularProgregessBar(data.percentage)}
+                    </div>
+                    <div className="col-6 align-middle">
+                      <h3 className="mt-20p ml-4 text-left">
+                        <b>{data.name}</b>
+                      </h3>
+                      <br />
+                      <h5 className="ml-15p text-darkorange">
+                        {data.percentage}% <i className="fa fa-caret-down"></i>
+                      </h5>
+                    </div>
+                  </div>
+                </div>
               ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="row mt-4">
-          {annualCarddata.map((data, index) => (
-            <div
-              className="col-md-6 col-lg-4 my-1 col-12 shadow mx-auto rounded10 bg-white"
-              key={index}
-              style={{ maxWidth: "400px" }}
-            >
-              <div className="p-2 row  m-1">
-                <div className="w-100"></div>
-                <div className="col-6">
-                  {circularProgregessBar(data.percentage)}
-                </div>
-                <div className="col-6 align-middle">
-                  <h3 className="mt-20p ml-4 text-left">
-                    <b>{data.name}</b>
-                  </h3>
+            </div>
+
+            <div className="row bg-primary mt-4 mx-3 border rounded10 px-4 shadow">
+              <div className="col-12 col-md-6 p-2 py-4">
+                <Map />
+              </div>
+              <div
+                className="col-12 col-md-6 p-2 py-4"
+                style={{ left: "24px" }}
+              >
+                <Map />
+              </div>
+            </div>
+
+            <div className="row mt-3 rounded">
+              {chartDatas.map((chartData, index) => (
+                <div
+                  className="col-12 col-lg-5 bg-white mx-auto  rounded10  border my-5 shadow "
+                  key={index}
+                >
+                  <div className="text-center text-primary py-4">
+                    <b style={{ fontSize: "3vmin" }}>
+                      {chartDatas[index].data.datasets[0].labelHead}
+                    </b>
+                  </div>
+
                   <br />
-                  <h5 className="ml-15p text-darkorange">
-                    {data.percentage}% <i className="fa fa-caret-down"></i>
-                  </h5>
+                  <div className="pt-4 pb-2">{Chart(chartData.data)}</div>
+                  <p className="text-darkorange ml-5 text-justify">
+                    <b>{chartData.desc}</b>
+                  </p>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-
-        <div className="row bg-primary mt-4 mx-3 border rounded10 px-4 shadow">
-          <div className="col-12 col-md-6 p-2 py-4">
-            <Map />
-          </div>
-          <div className="col-12 col-md-6 p-2 py-4" style={{ left: "24px" }}>
-            <Map />
-          </div>
-        </div>
-
-        <div className="row mt-3 rounded">
-          {chartDatas.map((chartData, index) => (
-            <div
-              className="col-12 col-lg-5 bg-white mx-auto  rounded10  border my-5 shadow "
-              key={index}
-            >
-              <div className="text-center text-primary py-4">
-                <b style={{ fontSize: "3vmin" }}>
-                  {chartDatas[index].data.datasets[0].labelHead}
-                </b>
-              </div>
-
-              <br />
-              <div className="pt-4 pb-2">{Chart(chartData.data)}</div>
-              <p className="text-darkorange ml-5 text-justify">
-                <b>{chartData.desc}</b>
-              </p>
-            </div>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </Base>
   );
